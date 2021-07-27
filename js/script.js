9e8ffa2e748612d104330df9ff023a2b6a701d27
Dylan McGee <dtmcgee@syr.edu>
2021-07-27 12:56:21 -0400

    Stuck Getting the proper model window to appear when selecting a card.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 const searchContainer = document.getElementById('search-container');
-
 const gallery = document.querySelector('#gallery');
-
+const cards = document.getElementsByClassName('card');
+const cardContainer = document.querySelector(".card-img-container");
+// console.log(cardContainer);
 
 /**  FETCH API **/
 
-function fetchData(url) {
-    return fetch(url)
+async function fetchData(url) {
+    return await fetch(url)
         //check status
         .then(response => response.json())
     //catch errors
 }
 
-
 fetchData('https://randomuser.me/api/?nat=us&results=12')
     .then(data => {
-         generateCard(data),
-        generateWindow(data);
+         generateCard(data);
+        //  generateWindow(data);
     })
     // .then(data => generateWindow(data))
 
+/**  FUNCTIONS **/
 
 function generateCard(data) {
     for (let i = 0; i < data.results.length; i++) {
@@ -37,53 +38,52 @@ function generateCard(data) {
             </div>
         `);
     }
+    //https://stackoverflow.com/questions/222841/most-efficient-way-to-convert-an-htmlcollection-to-an-array
+    [...cards].forEach(card => {
+        // console.log(card);
+        card.addEventListener('click', (e) => {
+            generateWindow(data[e.target]);
+        })
+    });
 }
-
-const modalContainer = document.createElement('div');
-modalContainer.className = 'modal-container';
-modalContainer.innerHTML = `
-<div class="modal-container">
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        <h3 id="name" class="modal-name cap">name</h3>
-        <p class="modal-text">email</p>
-        <p class="modal-text cap">city</p>
-        <hr>
-        <p class="modal-text">(555) 555-5555</p>
-        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        <p class="modal-text">Birthday: 10/21/2015</p>
-    </div>
-</div>
-`;
-
-const cardContainer = document.querySelector(".card-img-container");
-console.log(cardContainer);
 
 function generateWindow(data){
     let modalContainer = document.createElement('div');
     modalContainer.className = 'modal-container';
     gallery.insertAdjacentElement('afterend', modalContainer);
-    console.log(modalContainer);
-    let date = (data.results[0].dob.date);
-    day = date.split('-')[1];
-    month = date.split('-')[2];
-    month = month.substring(0,2);
-    year = date.split('-')[0];
+    console.log(data);
+      // console.log(modalContainer);
+    fixDate(data);
+    let info = data.results[0];
     modalContainer.innerHTML = `
         <div class="modal-container">
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${data.results[0].picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${data.results[0].name.first} ${data.results[0].name.last}</h3>
-                <p class="modal-text">${data.results[0].email}</p>
-                <p class="modal-text cap">${data.results[0].location.city}</p>
+                <img class="modal-img" src="${info.picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${info.name.first} ${info.name.last}</h3>
+                <p class="modal-text">${info.email}</p>
+                <p class="modal-text cap">${info.location.city}</p>
                 <hr>
-                <p class="modal-text">${data.results[0].phone}</p>
-                <p class="modal-text">${data.results[0].location.street.number} ${data.results[0].location.street.name}, ${data.results[0].location.city}, ${data.results[0].location.state} ${data.results[0].location.postcode}</p>
+                <p class="modal-text">${info.phone}</p>
+                <p class="modal-text">${info.location.street.number} ${info.location.street.name}, ${info.location.city}, ${info.location.state} ${info.location.postcode}</p>
                 <p class="modal-text">Birthday: ${day}/${month}/${year}</p>
             </div>
         </div>
-        `;}
+        `;
+        // console.log(data);
+    const closeBtn = document.getElementById('modal-close-btn');
+        // console.log(closeBtn);
+    closeBtn.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+    })
+}
+
+function fixDate(data){
+    let info = data.results[0];
+    let date = (info.dob.date);
+    day = date.split('-')[1];
+    month = date.split('-')[2];
+    month = month.substring(0,2);
+    year = date.split('-')[0];
+}
